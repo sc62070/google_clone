fd775dfb11b1eef832676d534decbe90d66ce716
Hyuk Jin Choi <sc62070@gmail.com>
2022-01-05 17:28:32 +0900

    google clone finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  content: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
-}
+};
